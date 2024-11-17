69497f0609c58d43580bee64ffb802aac7e1dcdd
Ekdvs <ekdvsampath02@gmail.com>
2024-11-17 12:41:01 +0530

    Add nowel changes into dev branch

--- src/main/java/com/flower/practical_02/Controller/StudentController.java
+++ src/main/java/com/flower/practical_02/Controller/StudentController.java
@@ -2,6 +2,8 @@ package com.flower.practical_02.Controller;
 import com.flower.practical_02.Model.Student;
 import com.flower.practical_02.ServiceImplementation.StudentServiceImplementation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
@@ -35,5 +37,27 @@ public class StudentController {
     @DeleteMapping("/deleteStudent/{id}")
     public void deleteStudent(@PathVariable("id") Long id) {
         studentService.deleteStudentById(id);
+    }
+
+    @GetMapping("/getByYear/{YearOfEnrollment}")
+    public ResponseEntity<List<Student>> getByYear(@PathVariable("YearOfEnrollment") String YearOfEnrollment) {
+        List<Student> student=studentService.getStudentsByEnrolledYear(YearOfEnrollment);
+        return new ResponseEntity<>(student, HttpStatus.OK);
+    }
+    @GetMapping("/getByDepartment/{id}")
+    public ResponseEntity<String> getByDepartmentById(@PathVariable("id") Long id) {
+        String department= studentService.getDepartmentNameById(id);
+        if(department!=null){
+            return ResponseEntity.ok(department);
+        }
+        else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/deleteByYear/{YearOfEnrollment}")
+    public ResponseEntity<String> removeStudentsByEnrolmentYear(@PathVariable("YearOfEnrollment")String YearOfEnrollment){
+        studentService.removeStudentsByEnrolmentYear(YearOfEnrollment);
+        return ResponseEntity.ok("Studnets enrolled in "+YearOfEnrollment+"have benn deleted");
     }
 }
